254a7cb94f96274becaead2d7e4ac9dfc34e90d5
wb-zd92954 <wb-zd929541@alibaba-inc.com>
2021-07-21 17:16:06 +0800

    添加docker容器化部署

--- cloud-application/src/main/java/com/dmj/cloud/CloudApplication.java
+++ cloud-application/src/main/java/com/dmj/cloud/CloudApplication.java
@@ -3,14 +3,12 @@ package com.dmj.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author zd
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan
 public class CloudApplication {
 
 	public static void main(String[] args) {
